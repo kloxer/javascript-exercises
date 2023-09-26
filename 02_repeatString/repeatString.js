cced175600d3bc3a66ba20415af9128edb2d1464
kloxer <102638907+kloxer@users.noreply.github.com>
2023-09-26 14:28:24 -0400

    Ex 2 and 3 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word,num) {
+if (num < 0){
+    return "ERROR";
+}
+if (word == "")
+{
+    return word;
+}
+var repeatedStr= "";
 
+for (let index = 0; index < num; index++) {
+    repeatedStr = repeatedStr + word;    
+}
+
+return repeatedStr;
 };
 
 // Do not edit below this line
