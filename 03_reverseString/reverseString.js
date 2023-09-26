52315a6cffcb5561b3647f2afef4794e6ce789f1
kloxer <102638907+kloxer@users.noreply.github.com>
2023-09-26 14:40:25 -0400

    Ex 3  complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let length = word.length;
+    let reversedStr = "";
+    for (let index = length -1; index >= 0; index--) {
+        reversedStr = reversedStr + word[index];
+        
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
