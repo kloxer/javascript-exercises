a64d1a209b4fc3868beb396856c00664117762d6
kloxer <102638907+kloxer@users.noreply.github.com>
2023-09-26 17:05:36 -0400

    Ex. 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (typeof a != "number" || typeof b != "number" )
+    {
+        return "ERROR";
+    }
+    if (a < 0 || b < 0)
+    {
+        return "ERROR";
+    }
+
+    let sum= 0;
+    let lower = a;
+    let higher = b;
+    if (a>b){
+        lower = b;
+        higher = a;
+    }
+    for (let index = lower; index <= higher; index++) {
+        sum = sum + index;
+        
+    }
+    return sum;
 
 };
 
